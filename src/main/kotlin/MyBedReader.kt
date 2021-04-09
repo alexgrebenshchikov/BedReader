df4efd7e6a07a2c38928d1bd13813c0569e9c254
alexgrebenshchikov <54818274+alexgrebenshchikov@users.noreply.github.com>
2021-04-09 21:57:07 +0300

    add some comments

--- src/main/kotlin/MyBedReader.kt
+++ src/main/kotlin/MyBedReader.kt
@@ -3,21 +3,24 @@ import kotlinx.serialization.*
 import kotlinx.serialization.json.*
 
 
+/**
+ * Index store map of (chromosome name to (start of entry, end of entry, number of line in BED file) ).
+ * */
 data class MyBedIndex(val chromosomeMap: Map<String, List<Triple<Int, Int, Int>>>) : BedIndex
 
 class MyBedReader : BedReader {
     override fun createIndex(bedPath: Path, indexPath: Path) {
         val chrMap: MutableMap<String, MutableList<Triple<Int, Int, Int>>> = mutableMapOf()
-        var n = 0
+        var numberOfLine = 0
         bedPath.toFile().forEachLine {
             val splitLine = it.split(""" +|\t""".toRegex())
             val chrName = splitLine.firstOrNull()
             if (chrName != null && chrName != "browser" && chrName != "track") {
                 if (!chrMap.containsKey(chrName))
                     chrMap[chrName] = mutableListOf()
-                chrMap[chrName]?.insertionSort(Triple(splitLine[1].toInt(), splitLine[2].toInt(), n))
+                chrMap[chrName]?.insertionSort(Triple(splitLine[1].toInt(), splitLine[2].toInt(), numberOfLine))
             }
-            n++
+            numberOfLine++
         }
         indexPath.toFile().writeText(Json.encodeToString(chrMap))
     }
@@ -33,20 +36,20 @@ class MyBedReader : BedReader {
         index: BedIndex, bedPath: Path,
         chromosome: String, start: Int, end: Int
     ): List<BedEntry> {
-        val listOfPieces = (index as MyBedIndex).chromosomeMap[chromosome] ?: return listOf()
-        var i = binSearch(listOfPieces, start)
+        val listOfEntries = (index as MyBedIndex).chromosomeMap[chromosome] ?: return listOf()
+        var i = binSearch(listOfEntries, start)
 
         val res: MutableList<BedEntry> = mutableListOf()
         val lines = bedPath.toFile().readLines().map { it.split(""" +|\t""".toRegex()).drop(3) }
 
-        while (i < listOfPieces.size && listOfPieces[i].first < end) {
-            if (listOfPieces[i].second <= end)
+        while (i < listOfEntries.size && listOfEntries[i].first < end) {
+            if (listOfEntries[i].second <= end)
                 res.add(
                     BedEntry(
                         chromosome,
-                        listOfPieces[i].first,
-                        listOfPieces[i].second,
-                        lines[listOfPieces[i].third]
+                        listOfEntries[i].first,
+                        listOfEntries[i].second,
+                        lines[listOfEntries[i].third]
                     )
                 )
             i++
